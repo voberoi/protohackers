9bc9c3609c9e45c5e1aeb73182cc1f4ee7652ba0
Vikram Oberoi <voberoi@gmail.com>
2022-09-16 09:50:45 -0400

    Smoke test.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use clap::{Parser, Subcommand};
+
+mod smoke_test;
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    SmokeTest {
+        client_or_server: String,
+        client_string: Option<String>,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    let args = Cli::parse();
+
+    match args.command {
+        Commands::SmokeTest {
+            client_or_server,
+            client_string,
+        } => match (client_or_server.as_str(), client_string) {
+            ("server", _) => smoke_test::run_server(),
+            ("client", Some(client_string)) => smoke_test::run_client(client_string.as_bytes()),
+            ("client", None) => smoke_test::run_client(b"Hello world!"),
+            _ => panic!("Invalid smoketest argument '{}'.", client_or_server),
+        },
+    }
 }
---
+++ src/smoke_test.rs
@@ -0,0 +1,50 @@
+use std::io::{BufReader, Read, Write};
+use std::net::{Shutdown, TcpListener, TcpStream};
+use std::str;
+
+use log::debug;
+use threadpool::ThreadPool;
+
+const NUM_WORKERS: usize = 5;
+
+pub fn run_server() {
+    let listener = TcpListener::bind("0.0.0.0:5001").unwrap();
+
+    let pool = ThreadPool::new(NUM_WORKERS);
+    for stream in listener.incoming() {
+        pool.execute(move || handle_connection(stream.unwrap()));
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    debug!("Handling a connection.");
+
+    debug!("Reading bytes sent by client.");
+    // Read bytes from the stream.
+    let mut reader = BufReader::new(&stream);
+    let mut buf = vec![];
+    reader.read_to_end(&mut buf).unwrap();
+
+    debug!("Writing bytes back.");
+    // Write them back, verbatim.
+    stream.write_all(&buf).unwrap();
+
+    debug!("Connection handled.");
+}
+
+pub fn run_client(bytes: &[u8]) {
+    debug!(
+        "Writing {} to server.",
+        String::from_utf8(bytes.to_vec()).unwrap()
+    );
+    let mut stream = TcpStream::connect("127.0.0.1:5001").unwrap();
+    stream.write_all(bytes).unwrap();
+    stream.shutdown(Shutdown::Write).unwrap();
+    debug!("Bytes written.");
+
+    debug!("Reading bytes from server.");
+    let mut reader = BufReader::new(&stream);
+    let mut buf = vec![];
+    let _bytes_read = reader.read_to_end(&mut buf).unwrap();
+    debug!("Bytes read: {}", str::from_utf8(&buf).unwrap());
+}
